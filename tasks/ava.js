1dfe0af7513060fd67c685eb80afb538d6800a58
Paulo Pires <taralloqueiroiz@gmail.com>
2016-04-05 05:35:14 -0700

    fixing failed build

--- tasks/ava.js
+++ tasks/ava.js
@@ -5,24 +5,23 @@ var Verbose = require('ava/lib/reporters/verbose');
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('ava', 'Run AVA tests', function () {
-
 		var cb = this.async();
 
-		var args =  this.filesSrc.concat('--color');
+		var args = this.filesSrc.concat('--color');
 
 		var api = new Api();
- 		var reporter = new Verbose();
-    var logs = '';
+		var reporter = new Verbose();
+		var logs = '';
 
- 		reporter.api = api;
+		reporter.api = api;
 
- 		api.on('test', function (test) {
-
+		api.on('test', function (test) {
 			logs += '\n' + reporter.test(test);
-	 	});
- 		api.on('error', function (error) {
+		});
+
+		api.on('error', function (error) {
 			logs += '\n' + reporter.unhandledError(error);
- 		});
+		});
 
 		api.run(args).then(function () {
 			logs += '\n' + reporter.finish();
@@ -30,13 +29,10 @@ module.exports = function (grunt) {
 			grunt.log.write('grunt-ava:\n' + logs);
 
 			cb();
-
 		}).catch(function (error) {
-
-			this.emit('error', new grunt.warn(error));
+			this.emit('error', grunt.warn(error));
 			cb();
 			return;
 		}.bind(this));
-
 	});
 };
